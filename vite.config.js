c382e2a50c0812d995693dbefe836059967b294b
Keerti Krishna Sreenivas S <2300031039@kluniversity.in>
2025-09-20 14:54:13 +0530

    commiting github actions

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CI-CD-Project-Frontend1/'
+   base: '/CI-CD-Project-Frontend1/'
+ 
 })
+
